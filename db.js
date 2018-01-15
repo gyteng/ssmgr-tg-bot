a185dde2b12fed7bd634c22447a4ec5666d7f116
gyt-Macmini <igyteng@gmail.com>
2018-01-15 21:21:31 +0800

    delete sticker

---
+++ db.js
@@ -0,0 +1,82 @@
+const knex = require('knex')({
+  client: 'sqlite3',
+  connection: {
+    filename: './data.sqlite',
+  },
+  useNullAsDefault: true,
+});
+
+const createTable = async () => {
+  return knex.schema.createTableIfNotExists('setting', function(table) {
+    table.string('key');
+    table.string('value');
+  }).then(() => {
+    return knex.schema.createTableIfNotExists('log', function(table) {
+      table.integer('is_deleted').defaultTo(0);
+      table.integer('update_id').primary();
+      table.integer('message_id');
+      table.integer('from_id');
+      table.integer('chat_id');
+      table.bigInteger('date');
+      table.string('text', 16384);
+      table.string('sticker', 16384);
+    });
+  });
+};
+
+createTable();
+
+const setUpdateId = async (id) => {
+  try {
+    const result = await knex('setting').select(['value']).where({key: 'updateId'});
+    if(result.length === 0) {
+      await knex('setting').insert({
+        key: 'updateId',
+        value: id || 1,
+      });
+    } else {
+      await knex('setting').where({key: 'updateId'}).update({
+        value: id,
+      });
+    }
+    return id;
+  } catch(err) {
+    return Promise.reject(err);
+  }
+};
+
+const getUpdateId = async () => {
+  try {
+    const result = await knex('setting').select(['value']).where({key: 'updateId'});
+    if(result.length === 0) {
+      return 1;
+    } else {
+      return result[0].value;
+    }
+  } catch(err) {
+    return Promise.reject(err);
+  }
+};
+
+const saveMessage = async (message) => {
+  await knex('log').insert({
+    update_id: message.update_id,
+    message_id: message.message.message_id,
+    from_id: message.message.from.id,
+    chat_id: message.message.chat.id,
+    date: message.message.date,
+    text: JSON.stringify(message.message.text),
+    sticker: JSON.stringify(message.message.sticker),
+  });
+};
+
+const deleteMessage = async (message) => {
+  await knex('log').update({
+    is_deleted: 1
+  }).where({ message_id: message.message.message_id });
+};
+
+exports.setUpdateId = setUpdateId;
+exports.getUpdateId = getUpdateId;
+exports.saveMessage = saveMessage;
+exports.deleteMessage = deleteMessage;
--- server.js
+++ server.js
@@ -1,1 +1,70 @@
 const token = require('./config').token;
+const db = require('./db');
+const rp = require('request-promise');
+const url = `https://api.telegram.org/bot${ token }/`;
+
+process.on('unhandledRejection', (reason, p) => {
+  console.error('Unhandled Rejection at: Promise', p, 'reason:', reason);
+});
+
+process.on('uncaughtException', (err) => {
+  console.error(`Caught exception:`);
+  console.error(err);
+});
+
+const deleteMessage = async (message) => {
+  try {
+    const result = await rp({
+      method: 'POST',
+      uri: url + 'deleteMessage',
+      form: {
+        chat_id: message.message.chat.id,
+        message_id: message.message.message_id,
+      },
+      simple: false,
+    });
+    await db.deleteMessage(message);
+  } catch (err) {
+
+  }
+};
+
+const getMessage = async () => {
+  const updateId = await db.getUpdateId();
+  try {
+    const result = await rp({
+      method: 'GET',
+      uri: url + 'getUpdates',
+      qs: {
+        offset: updateId,
+        timeout: 30,
+      },
+      simple: false,
+    });
+    const resultObj = JSON.parse(result);
+    if(resultObj.ok && resultObj.result.length) {
+      resultObj.result.forEach(async message => {
+        console.log(message);
+        await db.saveMessage(message);
+        if(message.message.sticker) {
+          deleteMessage(message);
+        }
+      });
+    }
+    if(resultObj.result.length) {
+      await db.setUpdateId(resultObj.result[resultObj.result.length - 1].update_id + 1);
+    }
+  } catch (err) {
+    return;
+  }
+};
+
+const pull = () => {
+  return getMessage()
+  .then(() => {
+    return pull();
+  }).catch(() => {
+    return pull();
+  });
+};
+pull();
