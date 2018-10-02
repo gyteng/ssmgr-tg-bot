36c66fca252f652b8e8262bbf363e3ac921ea4af
Gyt <igyteng@gmail.com>
2018-10-02 08:44:17 +0800

    spam list

--- src/server.ts
+++ src/server.ts
@@ -30,6 +30,53 @@ const deleteMessage = async (message: Message) => {
   });
 };
 
+const deleteUser = async (message: Message) => {
+  await db.deleteMessage(message);
+  return new Promise((resolve, reject) => {
+    post(url + 'kickChatMember', {
+      form: {
+        chat_id: message.getChatId(),
+        user_id: message.getFromId(),
+      }
+    }, (err, data, body) => {
+      if(err) {
+        return reject(err);
+      }
+      return resolve(body);
+    })
+  });
+}
+
+const sendMessage = async (text: String) => {
+  return new Promise((resolve, reject) => {
+    post(url + 'sendMessage', {
+      form: {
+        chat_id: -1001115671254,
+        text,
+      }
+    }, (err, data, body) => {
+      if(err) {
+        return reject(err);
+      }
+      return resolve(body);
+    })
+  });
+};
+
+const isSpam = (username: string) => {
+  const spamList = [
+    '去中心化交易所搬砖套利',
+    '电报拉人软件',
+    '批量投票上币',
+    'TG软件群私发',
+    '志玲成人视频',
+    '炸群服务',
+  ];
+  return spamList.some(spam => {
+    return username.trim().includes(spam.trim());
+  });
+};
+
 const getMessage = async () => {
   const updateId = await db.getUpdateId();
   const getMessages = (): any => {
@@ -58,10 +105,22 @@ const getMessage = async () => {
       if(msg.getSticker()) {
         const lastSticker = await db.getLastSticker(msg);
         if(lastSticker) {
-          if(msg.getDate() - lastSticker.date <= 30 || msg.getFromId() === lastSticker.from_id) {
+          if(msg.getDate() - lastSticker.date <= 45) {
+            await deleteMessage(msg);
+          } else if(msg.getDate() - lastSticker.date <= 150 && msg.getFromId() === lastSticker.from_id) {
             await deleteMessage(msg);
           }
         }
+      }
+      if(msg.getMessage().new_chat_member) {
+        const name = msg.getMessage().new_chat_member.first_name + msg.getMessage().new_chat_member.last_name;
+        if(isSpam(name)) {
+          await deleteMessage(msg);
+          await deleteUser(msg);
+        }
+      }
+      if(msg.getFromId() === 77956174 && msg.getChatId() === 77956174) {
+        await sendMessage(msg.getText());
       }
     });
     if(resultObj.result.length) {
